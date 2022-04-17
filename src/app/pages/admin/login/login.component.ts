e9b6c55310fc4dbce28b052097ff473a0fc9f9b0
RamazanM <r.ramazanmutlu@gmail.com>
2022-04-17 20:38:11 +0300

    Login success

--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -14,10 +14,13 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {}
   public login() {
     this.http
-      .post('https://ramazanmutlu.com/v1/auth/login', {
-        username: this.username,
-        password: this.password,
-      })
+      .post(
+        'https://us-central1-ramazanmutlu-222b5.cloudfunctions.net/api/v1/auth/login',
+        {
+          username: this.username,
+          password: this.password,
+        }
+      )
       .subscribe((d) => console.log(d));
   }
 }
